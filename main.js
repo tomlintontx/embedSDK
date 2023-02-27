923767502eaa1878db4978898009ecafd6079a42
Thomas Linton <thomaslinton@MACUSThasLinton.home>
2023-02-27 13:38:40 -0600

    changed the url to machine to https

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { SisenseFrame, enums } = window['sisense.embed'];
 const ele = document.getElementById('sisenseFrame');
 
 const sisenseFrame = new SisenseFrame({
-    url: 'http://54.173.244.240:30845',
+    url: 'https://tams.sisensepoc.com',
     dashboard: '63c02028d54e7100352b0e8d',
     settings: {
         showLeftPane: false,
