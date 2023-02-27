7e428c346e34cf7d7524f5f0d7c2e60fb178712d
Thomas Linton <thomaslinton@MACUSThasLinton.home>
2023-02-27 14:00:36 -0600

    trying out the two frames

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const { SisenseFrame, enums } = window['sisense.embed'];
 
 const ele = document.getElementById('sisenseFrame');
+const wEle = document.getElementById('widgetFrame');
 
 const sisenseFrame = new SisenseFrame({
     url: 'https://tams.sisensepoc.com',
@@ -18,3 +19,17 @@ sisenseFrame.render().then(() => {
   console.log("Sisense iFrame ready!");
 });
 
+function createWidget() {
+    const widgetFrame = new SisenseFrame({
+        url: 'https://tams.sisensepoc.com',
+        dashboard: '63c02028d54e7100352b0e8d',
+        settings: {
+            showToolbar: false,
+        },
+        element: wEle
+    });
+
+    widgetFrame.dashboard.createWidget()
+
+    widgetFrame.render(null,true)
+}
