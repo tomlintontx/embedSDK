8c477bbb807240226adeedbc2f2e4cd9ec02bae0
Thomas Linton <thomaslinton@MACUSThasLinton.home>
2023-02-27 15:38:56 -0600

    dfgafd

--- main.js
+++ main.js
@@ -21,19 +21,20 @@ sisenseFrame.render().then(() => {
   console.log("Sisense iFrame ready!");
 });
 
+const widgetFrame = new SisenseFrame({
+    url: 'https://tams.sisensepoc.com',
+    dashboard: '63c02028d54e7100352b0e8d',
+    settings: {
+        showToolbar: true,
+    },
+    element: wEle,
+    editMode: true
+});
 
 
 function createWidget() {
 
-    const widgetFrame = new SisenseFrame({
-        url: 'https://tams.sisensepoc.com',
-        dashboard: '63c02028d54e7100352b0e8d',
-        settings: {
-            showToolbar: true,
-        },
-        element: wEle,
-        editMode: true
-    });
+
     
     widgetFrame.render().then(() => {
       wEle.src = 'https://tams.sisensepoc.com/app/main/dashboards/63c02028d54e7100352b0e8d/widgets/new?datasource=New%20Jira&type=chart%2Fline';
