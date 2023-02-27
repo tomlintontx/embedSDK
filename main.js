0ddaad9b9265f40e592498c3345061e7a5870ae2
Thomas Linton <thomaslinton@MACUSThasLinton.home>
2023-02-27 15:12:59 -0600

    fadfa

--- main.js
+++ main.js
@@ -21,19 +21,6 @@ sisenseFrame.render().then(() => {
   console.log("Sisense iFrame ready!");
 });
 
-const widgetFrame = new SisenseFrame({
-    url: 'https://tams.sisensepoc.com',
-    dashboard: '63c02028d54e7100352b0e8d',
-    settings: {
-        showToolbar: true,
-    },
-    element: wEle,
-    editMode: true
-});
-
-widgetFrame.render().then(() => {
-  console.log("Sisense iFrame ready!");
-});
 
 
 function createWidget() {
