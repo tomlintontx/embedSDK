8ceab991c45f516b610a917c34b4dc10004bf356
Thomas Linton <thomaslinton@MACUSThasLinton.home>
2023-03-02 16:23:18 -0600

    dfgafd

--- main.js
+++ main.js
@@ -32,7 +32,10 @@ sisenseFrame.render().then(() => {
 
 function createWidget() {
 
-    window.frames[ele.id].location = 'https://tams.sisensepoc.com/app/main/dashboards/63c02028d54e7100352b0e8d/widgets/new?datasource=New%20Jira&type=chart%2Fline'
+    // window.frames[ele.id].location = 'https://tams.sisensepoc.com/app/main/dashboards/63c02028d54e7100352b0e8d/widgets/new?datasource=New%20Jira&type=chart%2Fline'
+
+
+    ele.src = 'https://tams.sisensepoc.com/app/main/dashboards/63c02028d54e7100352b0e8d/widgets/new?datasource=New%20Jira&type=chart%2Fline'
 
     // sisenseFrame.dashboard.createWidget();
 
